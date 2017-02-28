c15317ea9980500e76e6263774a87da0d045c9c4
Carl Lewis <carl@rapchat.me>
2017-02-28 16:25:15 -0500

    remove example methods

--- guidebox/resource.go
+++ guidebox/resource.go
@@ -73,7 +73,7 @@ func (client *GuideboxClient) GetShows(args ...interface{}) (result map[string]i
 	case tags:
 		params["tags"] = strings.Join(tags, ",")
 	}
-	
+
 	_, err = client.sling.New().Get(showsURL).QueryStruct(params).ReceiveSuccess(&result)
 
 	if err != nil{
@@ -99,114 +99,4 @@ func (client *GuideboxClient) GetItem(itemID int) (Item, error) {
 	return item, err
 }
 
-func (client *GuideboxClient) GetUser(userID string) (User, error) {
-	user := User{}
-	request_url := userURL + userID + ".json"
-	_, err := client.sling.New().Get(request_url).ReceiveSuccess(&user)
 
-	if err != nil {
-		return user, err
-	}
-
-	if user.ID == "" {
-		return user, errors.New("Invalid user ID, user does not exist or has no activity.")
-	}
-
-	return user, err
-}
-
-func (client *GuideboxClient) GetTopStories() ([]int, error) {
-	var topStories []int
-
-	_, err := client.sling.New().Get(topURL).ReceiveSuccess(&topStories)
-
-	if err != nil {
-		return topStories, err
-	}
-
-	return topStories, nil
-}
-
-func (client *GuideboxClient) GetNewStories() ([]int, error) {
-	var newStories []int
-
-	_, err := client.sling.New().Get(newURL).ReceiveSuccess(&newStories)
-
-	if err != nil {
-		return newStories, err
-	}
-
-	return newStories, nil
-}
-
-func (client *GuideboxClient) GetMaxItem() (int, error) {
-	var max int
-
-	_, err := client.sling.New().Get(maxItemURL).ReceiveSuccess(&max)
-
-	if err != nil {
-		return max, err
-	}
-
-	return max, nil
-}
-
-func (client *GuideboxClient) GetBestStories() ([]int, error) {
-	var bestStories []int
-
-	_, err := client.sling.New().Get(bestURL).ReceiveSuccess(&bestStories)
-
-	if err != nil {
-		return bestStories, err
-	}
-
-	return bestStories, nil
-}
-
-func (client *GuideboxClient) GetShowGuideboxStories() ([]int, error) {
-	var showGuideboxStories []int
-
-	_, err := client.sling.New().Get(showURL).ReceiveSuccess(&showGuideboxStories)
-
-	if err != nil {
-		return showGuideboxStories, err
-	}
-
-	return showGuideboxStories, nil
-}
-
-func (client *GuideboxClient) GetAskGuideboxStories() ([]int, error) {
-	var askGuideboxStories []int
-
-	_, err := client.sling.New().Get(askURL).ReceiveSuccess(&askGuideboxStories)
-
-	if err != nil {
-		return askGuideboxStories, err
-	}
-
-	return askGuideboxStories, nil
-}
-
-func (client *GuideboxClient) GetJobStories() ([]int, error) {
-	var jobStories []int
-
-	_, err := client.sling.New().Get(jobsURL).ReceiveSuccess(&jobStories)
-
-	if err != nil {
-		return jobStories, err
-	}
-
-	return jobStories, nil
-}
-
-func (client *GuideboxClient) GetUpdate() (Update, error) {
-	update := Update{}
-
-	_, err := client.sling.New().Get(updatesURL).ReceiveSuccess(&update)
-
-	if err != nil {
-		return update, err
-	}
-
-	return update, nil
-}
